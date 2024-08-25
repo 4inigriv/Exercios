f27c01cba0722951f77a606be0f33b33bf92cde3
Virgínia <virginiaguiarsantosigni@gmail.com>
2024-08-24 23:00:47 -0400

    The kth Factor of n

---
+++ Exercicios/beecrowd/easy/Amazon Spring '23 High Frequency/The_kth_Factor_of_n.py
@@ -0,0 +1,9 @@
+class Solution:
+    def kthFactor(self, n: int, k: int) -> int:
+        factors = []
+        for i in range(1, n + 1):
+            if n % i == 0:
+                factors.append(i)
+                if len(factors) == k:
+                    return factors[-1]
+        return -1
