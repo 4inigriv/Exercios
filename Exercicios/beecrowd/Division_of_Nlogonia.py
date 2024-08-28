bd0920e9743ce740aeef7b16cb69caad93e9d352
Virgínia <virginiaguiarsantosigni@gmail.com>
2024-08-27 20:55:28 -0400

    tratamento de erros do division of nlogonia

--- Exercicios/beecrowd/Division_of_Nlogonia.py
+++ Exercicios/beecrowd/Division_of_Nlogonia.py
@@ -1,17 +1,20 @@
 while True:
-    n, m = map(int, input().split()) #numero de compraçao da nova divisao
-    if n == 0 or m == 0:
-        break  # Sai do loop se o usuário inserir 0 0
-    x, y = map(int, input().split()) #coordenadas das cidades 
-
-    #o problema é um plano cartesiano mt simples
-    if x == n or y == m: #está na divisão
-        print("divisa")
-    elif x > n and y > m:
-        print("NE")  # Nordeste
-    elif x < n and y > m:
-        print("NO")  # Noroeste
-    elif x > n and y < m:
-        print("SE")  # Sudeste
-    elif x < n and y < m:
-        print("SO")  # Sudoeste
+    k = int(input())  # valor k 
+    if k == 0:  # termina se k for 0
+        break
+    
+    n, m = map(int, input().split())  # coordenadas do ponto divisório
+    
+    for i in range(k):
+        x, y = map(int, input().split())  # coordenadas da consulta
+        
+        if x == n or y == m:
+            print("divisa")
+        elif x > n and y > m:
+            print("NE")  # Nordeste
+        elif x < n and y > m:
+            print("NO")  # Noroeste
+        elif x > n and y < m:
+            print("SE")  # Sudeste
+        elif x < n and y < m:
+            print("SO")  # Sudoeste
