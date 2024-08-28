694e98135255cd87f1844e76e7f100328f584f8e
Virgínia <virginiaguiarsantosigni@gmail.com>
2024-08-28 17:36:37 -0400

    update

---
+++ Exercicios/beecrowd/Area.py
@@ -0,0 +1,11 @@
+a, b, c = map(float, input().split())
+triangulo = (a * c) / 2
+print(f"TRIANGULO: {triangulo:.3f}")
+circulo = 3.14159 * c**2
+print(f"CIRCULO: {circulo:.3f}")
+trapezio = ((a + b) * c) / 2
+print(f"TRAPEZIO: {trapezio:.3f}")
+quadrado = b * b
+print(f"QUADRADO: {quadrado:.3f}")
+retangulo = a * b
+print(f"RETANGULO: {retangulo:.3f}")
